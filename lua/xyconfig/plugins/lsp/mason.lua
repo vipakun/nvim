9981f94d777297ef4ff280adf4b0265b56ed7126
Xiangkun Yang <vipakun@hotmail.com>
2023-10-03 23:01:20 -0500

    remove null-ls and find replacement

--- lua/xyconfig/plugins/lsp/mason.lua
+++ lua/xyconfig/plugins/lsp/mason.lua
@@ -1,59 +1,58 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "jayp0521/mason-null-ls.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- import mason-null-ls
-    local mason_null_ls = require("mason-null-ls")
-    -- enable mason and configuration icons
-    mason.setup({
-      ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
-    })
+		-- import mason-tool-installer
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        -- "biome", -- typescript, js, css and html. TODO
-        "html",
-        "cssls",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "pyright",
-        "rust_analyzer",
-        "sqls",
-        "rust_analyzer",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+		-- enable mason and configuration icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_null_ls.setup({
-      -- list of formatters & linters for mason to install
-      ensure_installed = {
-        "prettier", -- ts/js formatter
-        "stylua", -- lua formatter
-        "eslint_d", -- ts/js linter
-        "pylint", -- python linter
-        "rust_analyzer", -- rust-analyzer
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true,
-    })
-  end,
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				-- "biome", -- typescript, js, css and html. TODO
+				"html",
+				"cssls",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"pyright",
+				"rust_analyzer",
+				"sqls",
+				"rust_analyzer",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+			},
+		})
+	end,
 }
